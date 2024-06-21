121d4a26e7552905db5653973f3b26093a041a83
Daniel-T-Henriques <danielt.henriques@gmail.com>
2024-06-21 15:18:18 -0300

    Added "Bhaskara"

---
+++ Bhaskara.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+	float a, b, c, delta, raiz_delta, x1, x2;
+	
+	printf("Insira o valor de a: ");
+	scanf("%f", &a);
+	printf("Insira o valor de b: ");
+	scanf("%f", &b);
+	printf("Insira o valor de c: ");
+	scanf("%f", &c);
+	
+	//Delta e raiz
+	delta = (b*b)-4*a*c;
+	raiz_delta = sqrt(delta);
+	
+	printf("\nDelta = %f\nRaiz de Delta = %f\n\n", delta, raiz_delta);
+	
+	//Duas opcoes para x
+	x1 = ((-b)+raiz_delta)/(2*a);
+	x2 = ((-b)-raiz_delta)/(2*a);
+	
+	//Printando resultados
+	if(isnan(x1) == 1 || isinf(x1) == 1){
+		printf("x1 nao existe");
+	}else{
+		printf("x1 = %f", x1);
+	}
+	
+	if(isnan(x2) == 1 || isinf(x2) == 1){
+		printf("\nx2 nao existe");
+	}else{
+		printf("\nx2 = %f", x2);
+	}
+}
